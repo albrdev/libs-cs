617dd1febe255f9720374f5dde3aa81a90e4d93b
albrdev <albrdev@gmail.com>
2019-07-03 01:31:15 +0200

    extended escape sequence formatter

---
+++ Text/Formatting/ExtendedNativeEscapeSequenceFormatter.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Libs.Text.Formatting
+{
+    public class ExtendedNativeEscapeSequenceFormatter : NativeEscapeSequenceFormatter
+    {
+        public enum NumericBaseType
+        {
+            Binary = 2,
+            Octal = 8,
+            Decimal = 10,
+            Hexadecimal = 16
+        }
+
+        public const NumericBaseType DefaultNumericBase = NumericBaseType.Hexadecimal;
+
+        public NumericBaseType NumericBase { get; set; } = DefaultNumericBase;
+
+        private static Dictionary<char, char> s_EscapeSequences = new Dictionary<char, char>
+        {
+            { '\x1B', 'e' }
+        };
+
+        private static Dictionary<char, char> s_UnescapeSequences = new Dictionary<char, char>
+        {
+            { 'e', '\x1B' }
+        };
+
+        protected override string Format()
+        {
+            char tmpChar;
+            if(s_EscapeSequences.TryGetValue(Current, out tmpChar))
+            {
+                return tmpChar.ToString();
+            }
+
+            if(!char.IsControl(Current) || NumericBase == NumericBaseType.Hexadecimal)
+                return base.Format();
+
+            switch(NumericBase)
+            {
+                case NumericBaseType.Binary:
+                    return $"B{System.Convert.ToString(Current, (int)NumericBase).PadLeft(8, '0')}";
+                case NumericBaseType.Octal:
+                    return $"o{System.Convert.ToString(Current, (int)NumericBase).PadLeft(3, '0')}";
+                case NumericBaseType.Decimal:
+                    return $"d{System.Convert.ToString(Current, (int)NumericBase).PadLeft(3, '0')}";
+                /*case NumericBaseType.Hexadecimal:
+                    identifier = 'x';
+                    break;*/
+                default:
+                    throw new System.ArgumentException($@"Numeric base not supported: '{NumericBase}'");
+            }
+        }
+
+        protected override string Parse()
+        {
+            char tmpChar;
+            if(s_UnescapeSequences.TryGetValue(Current, out tmpChar))
+            {
+                Next();
+                return tmpChar.ToString();
+            }
+
+            switch(Current)
+            {
+                case 'd':
+                {
+                    Next();
+                    string value = Next(3);
+                    if(value.Length != 3)
+                        throw new System.FormatException();
+
+                    return ((char)System.Convert.ToByte(value, 10)).ToString();
+                }
+                case 'B':
+                {
+                    Next();
+                    string value = Next(8);
+                    if(value.Length != 8)
+                        throw new System.FormatException();
+
+                    return ((char)System.Convert.ToByte(value, 2)).ToString();
+                }
+                case 'o':
+                {
+                    Next();
+                    string value = Next(3);
+                    if(value.Length != 3)
+                        throw new System.FormatException();
+
+                    return ((char)System.Convert.ToByte(value, 8)).ToString();
+                }
+                default:
+                    return base.Parse();
+            }
+        }
+
+        public string Escape(string text, NumericBaseType numericBase, bool numericBaseUpperCase = NativeEscapeSequenceFormatter.DefaultNumericBaseUpperCase)
+        {
+            return Escape(new StringReader(text), numericBase, numericBaseUpperCase);
+        }
+
+        public string Escape(TextReader reader, NumericBaseType numericBase, bool numericBaseUpperCase = NativeEscapeSequenceFormatter.DefaultNumericBaseUpperCase)
+        {
+            NumericBaseType tmpNumericBase = NumericBase;
+            NumericBase = numericBase;
+
+            string result = Escape(reader, numericBaseUpperCase);
+
+            NumericBase = tmpNumericBase;
+            return result;
+        }
+
+        public ExtendedNativeEscapeSequenceFormatter(NumericBaseType numericBase = DefaultNumericBase, bool numericBaseUpperCase = NativeEscapeSequenceFormatter.DefaultNumericBaseUpperCase) : base(numericBaseUpperCase)
+        {
+            NumericBase = numericBase;
+        }
+    }
+}
