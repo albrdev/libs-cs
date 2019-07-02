72871a42c9665eb1c7596b71263e2e7f0ec32738
albrdev <albrdev@gmail.com>
2019-07-03 01:32:26 +0200

    extension(classes) project added

---
+++ Extensions/IntExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Libs.Extensions
+{
+    public static class IntExtensions
+    {
+        public static int Length(this int self, int radix = 10)
+        {
+            if(radix < 2)
+                throw new System.ArgumentException();
+
+            if(self == 0)
+                return 1;
+
+            int count = 0;
+            while(self != 0)
+            {
+                self /= radix;
+                count++;
+            }
+
+            return count;
+        }
+
+        public static bool IsPowerOfTwo(this int self)
+        {
+            self = Math.Abs(self);
+            return self != 0 && (self & (self - 1)) == 0;
+        }
+    }
+}
