b0440981c1f17a552db0243c5ac928ab8ffc9c3a
albrdev <albrdev@gmail.com>
2019-07-03 18:12:59 +0200

    test priority class

---
+++ UnitTests/TestPriority/PriorityOrderer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Sdk;
+using Xunit.Abstractions;
+using Libs.Extensions;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+namespace UnitTests.TestPriority
+{
+    public class PriorityComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if(x == 0 && y == 0)
+                return 0;
+
+            if(x == 0)
+                return 1;
+            else if(x < 0)
+                x = Math.Abs(int.MinValue - x);
+
+            if(y == 0)
+                return -1;
+            else if(y < 0)
+                y = Math.Abs(int.MinValue - y);
+
+            return x.CompareTo(y);
+        }
+    }
+
+    public class PriorityOrderer : ITestCaseOrderer
+    {
+        public IEnumerable<T> OrderTestCases<T>(IEnumerable<T> testCases) where T : ITestCase
+        {
+            var sortedTestCases = new SortedDictionary<int, List<T>>(new PriorityComparer());
+
+            foreach(var testCase in testCases)
+            {
+                IAttributeInfo attribute = testCase.TestMethod.Method.GetCustomAttributes((typeof(TestPriorityAttribute))).FirstOrDefault();
+                sortedTestCases.GetOrAdd(attribute != null ? attribute.GetNamedArgument<int>("Priority") : 0).Add(testCase);
+            }
+
+            foreach(var list in sortedTestCases.Values)
+            {
+                list.Sort((x, y) => StringComparer.OrdinalIgnoreCase.Compare(x.TestMethod.Method.Name, y.TestMethod.Method.Name));
+                foreach(var testCase in list)
+                {
+                    yield return testCase;
+                }
+            }
+        }
+    }
+}
---
+++ UnitTests/TestPriority/TestPriorityAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UnitTests.TestPriority
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class TestPriorityAttribute : System.Attribute
+    {
+        public int Priority { get; private set; } = 0;
+
+        public TestPriorityAttribute(int priority)
+        {
+            Priority = priority;
+        }
+    }
+}
