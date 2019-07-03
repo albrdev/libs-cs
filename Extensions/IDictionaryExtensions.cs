bbd7392362cc98f9d43b1622d24a25dd7e42ae36
albrdev <albrdev@gmail.com>
2019-07-03 18:09:42 +0200

    IDictionary extensions

---
+++ Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Libs.Extensions
+{
+    public static class IDictionaryExtensions
+    {
+        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key) where TValue : new()
+        {
+            if(self == null)
+                throw new System.ArgumentNullException("self");
+
+            if(!self.TryGetValue(key, out var value))
+            {
+                value = new TValue();
+                self.Add(key, value);
+            }
+
+            return value;
+        }
+    }
+}
