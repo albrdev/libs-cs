21e9902ea4a0d9c6f618c2a3f01bea5543019af3
albrdev <albrdev@gmail.com>
2019-07-08 01:42:09 +0200

    swapped output order

--- UnitTests/ExpressionParserTest.cs
+++ UnitTests/ExpressionParserTest.cs
@@ -23,8 +23,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"1 + 10^2^3");
             double cmp = 1 + (double)ExpressionParserTestFixture.Exponentiation(10, ExpressionParserTestFixture.Exponentiation(2, 3));
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             m_Fixture.ResultVariables.Add(res);
             Assert.Equal(cmp, res);
@@ -36,8 +36,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"3 + --4");
             double cmp = 3 + -(-4);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             m_Fixture.ResultVariables.Add(res);
             Assert.Equal(cmp, res);
@@ -49,8 +49,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"2math.pi");
             double cmp = 2 * Math.PI;
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             m_Fixture.ResultVariables.Add(res);
             Assert.Equal(cmp, res);
@@ -62,8 +62,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"3 + 4 * 2 / (1 - 5) ^ 2 ^ 3");
             double cmp = 3 + 4 * 2 / (double)ExpressionParserTestFixture.Exponentiation((1 - 5), ExpressionParserTestFixture.Exponentiation(2, 3));
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             m_Fixture.ResultVariables.Add(res);
             Assert.Equal(cmp, res);
@@ -75,8 +75,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"sin(max(2, 3) / 3 * math.pi)");
             double cmp = Math.Sin((double)ExpressionParserTestFixture.Max(2, 3) / 3 * Math.PI);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             m_Fixture.ResultVariables.Add(res);
             Assert.Equal(cmp, res);
@@ -88,8 +88,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"4 + 5 * (5 + 2)");
             double cmp = 4 + 5 * (5 + 2);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -100,8 +100,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"bla = (1 + 1) * 2");
             double cmp = (1 + 1) * 2;
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -112,8 +112,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"bla * 5");
             double cmp = 2 * 5;
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -124,8 +124,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"bla2 = -(1 + 1) * 4 + -bla2");
             double cmp = -(1 + 1) * 4 + -(-2);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -136,8 +136,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"bla3 = (10^2^3) + bla3");
             double cmp = (double)ExpressionParserTestFixture.Exponentiation(10, ExpressionParserTestFixture.Exponentiation(2, 3)) + (double)ExpressionParserTestFixture.Exponentiation(10, ExpressionParserTestFixture.Exponentiation(2, 3));
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -148,8 +148,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"2^1da^-5");
             double cmp = (double)ExpressionParserTestFixture.Exponentiation(2, ExpressionParserTestFixture.Exponentiation((1 * 10), -5));
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -160,8 +160,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"-(-5)");
             double cmp = -(-5);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -172,8 +172,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"min(5, -5)");
             double cmp = (double)ExpressionParserTestFixture.Min(5, -5);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -184,8 +184,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"2 ** 3");
             double cmp = (double)ExpressionParserTestFixture.Exponentiation(2, 3);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -196,8 +196,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"10 / 3");
             double cmp = 10.0 / 3.0;
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -208,8 +208,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"10 // 3");
             double cmp = Math.Truncate(10.0 / 3.0);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -220,8 +220,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"1da^3 * (10 - 2 * 2)");
             double cmp = (double)ExpressionParserTestFixture.Exponentiation((1 * 10), 3) * (10 - 2 * 2);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -232,8 +232,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"(1 + 2) * 5 # comment");
             double cmp = (1 + 2) * 5;
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -244,8 +244,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"(phys.au / phys.c) / 60 # How long light travels from the Sun to Earth");
             double cmp = (System.Convert.ToDouble(m_Fixture.ArithmeticVariables["phys.au"].Value) / System.Convert.ToDouble(m_Fixture.ArithmeticVariables["phys.c"].Value)) / 60;
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -256,8 +256,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"384402000 / phys.c # How long light travels from the Moon to Earth");
             double cmp = 384402000 / System.Convert.ToDouble(m_Fixture.ArithmeticVariables["phys.c"].Value);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -268,8 +268,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"-10^3");
             double cmp = (double)ExpressionParserTestFixture.Exponentiation(-10, 3);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -280,8 +280,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"-10^3");
             double cmp = -(double)ExpressionParserTestFixture.Exponentiation(10, 3);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -292,8 +292,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"1 + -(10^3)");
             double cmp = 1 + -(double)ExpressionParserTestFixture.Exponentiation(10, 3);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -304,8 +304,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"x * x - y * y", ("x", 10), ("y", 5));
             double cmp = 10 * 10 - 5 * 5;
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -316,8 +316,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"ans(0) + ans(-4) - ans()");
             double cmp = System.Convert.ToDouble(m_Fixture.Ans(0)) + System.Convert.ToDouble(m_Fixture.Ans(-4)) - System.Convert.ToDouble(m_Fixture.Ans());
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -328,8 +328,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"1 + pi() * 2");
             double cmp = 1 + System.Convert.ToDouble(ExpressionParserTestFixture.PI()) * 2;
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -340,8 +340,8 @@ namespace UnitTests
             var res = m_Fixture.ArithmeticExpressionParser.Evaluate(@"""abc"" + ""123""");
             var cmp = $@"abc123";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -352,8 +352,8 @@ namespace UnitTests
             var res = m_Fixture.BitwiseExpressionParser.Evaluate(@"1 + 0 * 1");
             int cmp = 1 | 0 & 1;
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -364,8 +364,8 @@ namespace UnitTests
             var res = m_Fixture.BitwiseExpressionParser.Evaluate(@"1 ^ (1 * 1) * 0");
             int cmp = 1 ^ (1 & 1) & 0;
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -376,8 +376,8 @@ namespace UnitTests
             var res = m_Fixture.BitwiseExpressionParser.Evaluate(@"!(0 + 0) ^ !(1 * 1)");
             int cmp = ~(0 | 0) ^ ~(1 & 1);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -388,8 +388,8 @@ namespace UnitTests
             var res = m_Fixture.BitwiseExpressionParser.Evaluate(@"1 << 10");
             int cmp = 1 << 10;
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -400,8 +400,8 @@ namespace UnitTests
             var res = m_Fixture.BitwiseExpressionParser.Evaluate(@"(1 << 10) * 1023");
             int cmp = (1 << 10) & 1023;
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -412,8 +412,8 @@ namespace UnitTests
             var res = m_Fixture.BitwiseExpressionParser.Evaluate(@"1 ^ 1(1 + 0)");
             int cmp = 1 ^ 1 & (1 | 0);
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
--- UnitTests/TextFormatterTest.cs
+++ UnitTests/TextFormatterTest.cs
@@ -23,8 +23,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"abc 123 def");
             var cmp = $@"abc 123 def";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -35,8 +35,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"abc $${123()} $$$${def}");
             var cmp = $@"abc ${{123()}} $${{def}}";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -47,8 +47,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"${strargs(""123\'\t\\\v\""\n456"")}");
             var cmp = $@"{TextFormatterTestFixture.StrArgs("123\'\t\\\v\"\n456")}";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -59,8 +59,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"Array indices: ${array(0)}, ${array(2)}, ${array(4)}, ${sum(1, 2, 3, 4, 5)}");
             var cmp = $@"Array indices: {TextFormatterTestFixture.Array(0)}, {TextFormatterTestFixture.Array(2)}, {TextFormatterTestFixture.Array(4)}, {TextFormatterTestFixture.Sum(1, 2, 3, 4, 5)}";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -71,8 +71,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"${argcnt(1, 2, 3)}${argcnt(1, 2)}");
             var cmp = $@"{TextFormatterTestFixture.ArgumentCount(1, 2, 3)}{TextFormatterTestFixture.ArgumentCount(1, 2)}";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -83,8 +83,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"${sum(1, 2, 3, 4, 5)}, ${argcnt(1, 2, 3, ""abc"", ""def"")} ${strlen(""foo bar baz"")})${strargs(123.456, ""foo bar baz"")}""${array(0)}"" ${pi}, ${pi})${pi}${pi}""${pi}""");
             var cmp = $@"{TextFormatterTestFixture.Sum(1, 2, 3, 4, 5)}, {TextFormatterTestFixture.ArgumentCount(1, 2, 3, "abc", "def")} {TextFormatterTestFixture.StringLength("foo bar baz")}){TextFormatterTestFixture.StrArgs(123.456, "foo bar baz")}""{TextFormatterTestFixture.Array(0)}"" {Math.PI}, {Math.PI}){Math.PI}{Math.PI}""{Math.PI}""";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -95,8 +95,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"${sum(1, 2, 3, 4, sum(1, 2, 3, 4))} ${strlen(""foo bar baz"")}");
             var cmp = $@"{TextFormatterTestFixture.Sum(1, 2, 3, 4, TextFormatterTestFixture.Sum(1, 2, 3, 4))} {TextFormatterTestFixture.StringLength("foo bar baz")}";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -107,8 +107,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"${sum(1, argcnt(1, 2, 3), strlen(""foo bar baz""), pi)}");
             var cmp = $@"{TextFormatterTestFixture.Sum(1, TextFormatterTestFixture.ArgumentCount(1, 2, 3), TextFormatterTestFixture.StringLength("foo bar baz"), Math.PI)}";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -119,8 +119,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"hej $$${sum(100, 123.456, -50, argcnt(4, 5, 6))} lol ${strargs(""bla\""\n\t\\\"""", 123)} lol $$bla $${bla}");
             var cmp = $@"hej $176.456 lol str=""{"bla\"\n\t\\\", 123"}"" lol $bla ${{bla}}";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -131,8 +131,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"hej ${sum(1, 2, 3) + argcnt(1, 2, 3)}bla${1 + ""bla"" + 2}$${bla}");
             var cmp = $@"hej {$"{TextFormatterTestFixture.Sum(1, 2, 3)}{TextFormatterTestFixture.ArgumentCount(1, 2, 3)}"}bla{"1bla2"}${{bla}}";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -143,8 +143,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"${1+-2}");
             var cmp = $@"1-2";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -155,8 +155,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"${pi + (""pi"" + pi + ""bla"")}");
             var cmp = $@"{Math.PI}pi{Math.PI}bla";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
@@ -167,8 +167,8 @@ namespace UnitTests
             var res = m_Fixture.TextFormatter.Format(@"${strlen(""abcdefghijklmnopqrstuvwxyz"") + strlen(""0123456789"")}");
             var cmp = $@"{TextFormatterTestFixture.StringLength("abcdefghijklmnopqrstuvwxyz")}{TextFormatterTestFixture.StringLength("0123456789")}";
 
-            m_Output.WriteLine($@"""{ res }""");
             m_Output.WriteLine($@"""{ cmp }""");
+            m_Output.WriteLine($@"""{ res }""");
 
             Assert.Equal(cmp, res);
         }
