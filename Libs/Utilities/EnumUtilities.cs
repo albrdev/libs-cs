5bd816ea1ab26d27bd6d38c2beb0526b2eaeaf79
albrdev <albrdev@gmail.com>
2019-07-18 15:29:22 +0200

    utilities project added

---
+++ Libs/Utilities/EnumUtilities.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilities
+{
+    public static class EnumUtilities
+    {
+        public static bool IsDefined<TEnum>(object value) where TEnum : struct, IConvertible
+        {
+            return Enum.IsDefined(typeof(TEnum), value);
+        }
+
+        public static IEnumerable<TEnum> GetValues<TEnum>() where TEnum : struct, IConvertible
+        {
+            return Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+        }
+
+        public static IEnumerable<string> GetNames<TEnum>() where TEnum : struct, IConvertible
+        {
+            return Enum.GetNames(typeof(TEnum));
+        }
+
+        public static IEnumerable<KeyValuePair<TEnum, string>> GetKeyValuePairs<TEnum>() where TEnum : struct, IConvertible
+        {
+            return Enum.GetValues(typeof(TEnum)).Cast<TEnum>().Select(v => new KeyValuePair<TEnum, string>(v, v.ToString()));
+        }
+
+        #region Parse
+        public static TEnum Parse<TEnum>(string value) where TEnum : struct, IConvertible
+        {
+            TEnum result = (TEnum)Enum.Parse(typeof(TEnum), value);
+            if(!EnumUtilities.IsDefined<TEnum>(value))
+                throw new System.ArgumentOutOfRangeException();
+
+            return result;
+        }
+
+        public static TEnum Parse<TEnum>(int value) where TEnum : struct, IConvertible
+        {
+            if(!EnumUtilities.IsDefined<TEnum>(value))
+                throw new System.ArgumentOutOfRangeException();
+
+            return (TEnum)((object)value);
+        }
+        #endregion
+
+        #region TryParse
+        public static bool TryParse<TEnum>(string value, out TEnum result) where TEnum : struct, IConvertible
+        {
+            return Enum.TryParse(value, out result) ? !EnumUtilities.IsDefined<TEnum>(value) : false;
+        }
+
+        public static bool TryParse<TEnum>(int value, out TEnum result) where TEnum : struct, IConvertible
+        {
+            if(EnumUtilities.IsDefined<TEnum>(value))
+            {
+                result = (TEnum)((object)value);
+                return true;
+            }
+            else
+            {
+                result = default;
+                return false;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Libs/Utilities/PathUtilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+public static class PathUtilities
+{
+    public static string FullPath(params string[] subPaths)
+    {
+        string result = string.Empty;
+        foreach(var subPath in subPaths)
+        {
+            result = Path.Combine(result, subPath);
+        }
+
+        return result;
+    }
+}
